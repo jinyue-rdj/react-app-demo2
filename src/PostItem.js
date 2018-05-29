3831044412bb64a42d2645c59a44fec386e86926
duojun.rdj@alibaba-inc.com <rdj2432903@126.com>
2018-05-30 00:01:33 +0800

    commit

--- src/PostItem.js
+++ src/PostItem.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./PostItem.css"
+import "./PostItem.css";
+import like from "./images/like-default.png";
 
 export default function PostItem(props){
     const handleClick = () =>{
@@ -9,12 +10,12 @@ export default function PostItem(props){
     const post = props.post;
 
     return (
-        <li>
-            <div>{post.title}</div>
+        <li className='item'>
+            <div className='title'>{post.title}</div>
             <div>创建人：<span>{post.author}</span></div>
             <div>创建时间<span>{post.date}</span></div>
-            <div>
-                <button onClick={handleClick}>点赞</button>
+            <div className='like'>
+                <span><img src={like} onClick={handleClick} /></span>
                 <span>{post.vote}</span>
             </div>
         </li>
